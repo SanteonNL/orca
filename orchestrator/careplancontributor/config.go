3a518b860e795afa96ab3c4ee05a011845216da1
Rein Krul <info@reinkrul.nl>
2024-06-27 16:14:25 +0200

    removed dead code

--- orchestrator/careplancontributor/config.go
+++ orchestrator/careplancontributor/config.go
@@ -2,11 +2,3 @@ package careplancontributor
 
 type Config struct {
 }
-
-type AppLaunchConfig struct {
-	Demo DemoAppLaunchConfig
-}
-
-type DemoAppLaunchConfig struct {
-	Enabled bool `koanf:"enabled"`
-}
