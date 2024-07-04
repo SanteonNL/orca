6fd8a944bb8f186af8207e41618be874e1c4029d
Rein Krul <info@reinkrul.nl>
2024-07-04 13:08:06 +0200

    Configure max body response size

--- orchestrator/lib/coolfhir/client.go
+++ orchestrator/lib/coolfhir/client.go
@@ -11,9 +11,9 @@ type ClientCreator func(properties map[string]string) fhirclient.Client
 var ClientFactories = map[string]ClientCreator{}
 
 func Config() *fhirclient.Config {
-	return &fhirclient.Config{
-		Non2xxStatusHandler: func(response *http.Response, responseBody []byte) {
-			log.Debug().Msgf("Non-2xx status code from FHIR server (code=%d, url=%s), content: %s", response.StatusCode, response.Request.URL, string(responseBody))
-		},
+	config := fhirclient.DefaultConfig()
+	config.Non2xxStatusHandler = func(response *http.Response, responseBody []byte) {
+		log.Debug().Msgf("Non-2xx status code from FHIR server (%s %s, status=%d), content: %s", response.Request.Method, response.Request.URL, response.StatusCode, string(responseBody))
 	}
+	return &config
 }
