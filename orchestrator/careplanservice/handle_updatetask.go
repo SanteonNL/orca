962f31ae28f0b85677eb30ef9d574fc438f62a19
david-welman <dwelman@xebia.com>
2024-08-23 13:45:10 +0200

    INT-238: Implement state transition validation for Task in CPS
    Adds all necessary transition logic and testing for transitions between all states.
    
    Future work to be done: Implement the update API and handler

--- orchestrator/careplanservice/handle_updatetask.go
+++ orchestrator/careplanservice/handle_updatetask.go
@@ -52,8 +52,65 @@ func (s *Service) handleUpdateTask(httpResponse http.ResponseWriter, httpRequest
 			// Just respond with the original Task that was sent.
 			httpResponse.WriteHeader(http.StatusOK)
 			return json.NewEncoder(httpResponse).Encode(task)
+// TODO: Implement UpdateTask handler
+
+func isValidTransition(from fhir.TaskStatus, to fhir.TaskStatus, isOwner bool, isRequester bool) bool {
+	if isOwner == false && isRequester == false {
+		return false
+	}
+	// Transitions valid for owner only
+	if isOwner {
+		if from == fhir.TaskStatusRequested && to == fhir.TaskStatusReceived {
+			return true
+		}
+		if from == fhir.TaskStatusRequested && to == fhir.TaskStatusAccepted {
+			return true
+		}
+		if from == fhir.TaskStatusRequested && to == fhir.TaskStatusRejected {
+			return true
+		}
+		if from == fhir.TaskStatusReceived && to == fhir.TaskStatusAccepted {
+			return true
+		}
+		if from == fhir.TaskStatusReceived && to == fhir.TaskStatusRejected {
+			return true
+		}
+		if from == fhir.TaskStatusAccepted && to == fhir.TaskStatusInProgress {
+			return true
+		}
+		if from == fhir.TaskStatusInProgress && to == fhir.TaskStatusCompleted {
+			return true
+		}
+		if from == fhir.TaskStatusInProgress && to == fhir.TaskStatusFailed {
+			return true
+		}
+		if from == fhir.TaskStatusReady && to == fhir.TaskStatusCompleted {
+			return true
 		}
 		return fmt.Errorf("failed to update Task (CareTeam updated=%v): %w", careTeamUpdated, err)
 	}
 	return nil
+		if from == fhir.TaskStatusReady && to == fhir.TaskStatusFailed {
+			return true
+		}
+	}
+	// Transitions valid for owner or requester
+	if isOwner || isRequester {
+		if from == fhir.TaskStatusRequested && to == fhir.TaskStatusCancelled {
+			return true
+		}
+		if from == fhir.TaskStatusReceived && to == fhir.TaskStatusCancelled {
+			return true
+		}
+		if from == fhir.TaskStatusAccepted && to == fhir.TaskStatusCancelled {
+			return true
+		}
+		if from == fhir.TaskStatusInProgress && to == fhir.TaskStatusOnHold {
+			return true
+		}
+		if from == fhir.TaskStatusOnHold && to == fhir.TaskStatusInProgress {
+			return true
+		}
+	}
+	return false
 }
---
+++ orchestrator/careplanservice/handle_updatetask_test.go
@@ -0,0 +1,387 @@
+package careplanservice
+
+import (
+	"testing"
+
+	"github.com/samply/golang-fhir-models/fhir-models/fhir"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_isValidTransition(t *testing.T) {
+	type args struct {
+		from        fhir.TaskStatus
+		to          fhir.TaskStatus
+		isOwner     bool
+		isRequester bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// Positive cases
+		{
+			name: "requested -> received : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusReceived,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "requested -> accepted : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusAccepted,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "requested -> rejected : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusRejected,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "requested -> cancelled : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "requested -> cancelled : requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "requested -> cancelled : owner & requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "received -> accepted : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusAccepted,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "received -> rejected : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusRejected,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "received -> cancelled : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "received -> cancelled : requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "received -> cancelled : owner & requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "accepted -> in-progress : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusAccepted,
+				to:          fhir.TaskStatusInProgress,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "accepted -> cancelled : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusAccepted,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "accepted -> cancelled : requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusAccepted,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "accepted -> cancelled : owner & requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusAccepted,
+				to:          fhir.TaskStatusCancelled,
+				isOwner:     true,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "in-progress -> completed : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusCompleted,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "in-progress -> failed : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusFailed,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "in-progress -> on-hold : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusOnHold,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "in-progress -> on-hold : requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusOnHold,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "in-progress -> on-hold : owner & requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusOnHold,
+				isOwner:     true,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "on-hold -> in-progress : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusOnHold,
+				to:          fhir.TaskStatusInProgress,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "on-hold -> in-progress : requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusOnHold,
+				to:          fhir.TaskStatusInProgress,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "on-hold -> in-progress : owner & requester (OK)",
+			args: args{
+				from:        fhir.TaskStatusOnHold,
+				to:          fhir.TaskStatusInProgress,
+				isOwner:     true,
+				isRequester: true,
+			},
+			want: true,
+		},
+		{
+			name: "ready -> completed : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReady,
+				to:          fhir.TaskStatusCompleted,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		{
+			name: "ready -> failed : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReady,
+				to:          fhir.TaskStatusFailed,
+				isOwner:     true,
+				isRequester: false,
+			},
+			want: true,
+		},
+		// Negative cases -> Invalid requester/owner
+		{
+			name: "requested -> received : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusReceived,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "requested -> accepted : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusAccepted,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "requested -> rejected : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusRequested,
+				to:          fhir.TaskStatusRejected,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "received -> accepted : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusAccepted,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "received -> rejected : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusReceived,
+				to:          fhir.TaskStatusRejected,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "accepted -> in-progress : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusAccepted,
+				to:          fhir.TaskStatusInProgress,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "in-progress -> completed : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusCompleted,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "in-progress -> failed : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusInProgress,
+				to:          fhir.TaskStatusFailed,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "ready -> completed : requester (FAIL)",
+			args: args{
+				from:        fhir.TaskStatusReady,
+				to:          fhir.TaskStatusCompleted,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+		{
+			name: "ready -> failed : owner (OK)",
+			args: args{
+				from:        fhir.TaskStatusReady,
+				to:          fhir.TaskStatusFailed,
+				isOwner:     false,
+				isRequester: true,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isValidTransition(tt.args.from, tt.args.to, tt.args.isOwner, tt.args.isRequester)
+			require.Equal(t, tt.want, got)
+			// Validate that the opposite direction fails, besides in-progress <-> on-hold
+			if (tt.args.from == fhir.TaskStatusOnHold && tt.args.to == fhir.TaskStatusInProgress) || (tt.args.from == fhir.TaskStatusInProgress && tt.args.to == fhir.TaskStatusOnHold) {
+				return
+			}
+			got = isValidTransition(tt.args.to, tt.args.from, tt.args.isOwner, tt.args.isRequester)
+			require.Equal(t, false, got)
+		})
+	}
+}
