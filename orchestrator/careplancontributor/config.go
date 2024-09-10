653bed98599efc636ecc6a60567ec0aed5135ce2
david-welman <dwelman@xebia.com>
2024-09-10 13:04:16 +0200

    INT-241: Add FHIR proxy for CPC to CPC calls
    Adds a FHIR proxy endpoint for method calls from one CPC instance to another.
    
    Still to do: Auth based on provided CareTeam

--- orchestrator/careplancontributor/config.go
+++ orchestrator/careplancontributor/config.go
@@ -5,6 +5,7 @@ import "errors"
 type Config struct {
 	CarePlanService CarePlanServiceConfig `koanf:"careplanservice"`
 	FrontendConfig  FrontendConfig        `koanf:"frontend"`
+	FHIR            FHIRConfig            `koanf:"fhir"`
 	Enabled         bool                  `koanf:"enabled"`
 }
 
@@ -26,4 +27,17 @@ type CarePlanServiceConfig struct {
 type FrontendConfig struct {
 	// URL is the base URL of the frontend for ORCA
 	URL string `koanf:"url"`
+}
+
+type FHIRConfig struct {
+	// BaseURL is the base URL of the FHIR server to connect to.
+	BaseURL string `koanf:"url"`
+	// Auth is the authentication configuration for the FHIR server.
+	Auth FHIRAuthConfig `koanf:"auth"`
+}
+
+type FHIRAuthConfig struct {
+	// Type of authentication to use, supported options: azure-managedidentity.
+	// Leave empty for no authentication.
+	Type string `koanf:"type"`
 }
