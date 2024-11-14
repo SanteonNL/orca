f68e3dc9675b76abbb04b5c33050972e23ff2511
david-welman <dwelman@xebia.com>
2024-11-14 10:47:41 +0100

    Fix imports

--- orchestrator/careplanservice/handle_getcareplan_test.go
+++ orchestrator/careplanservice/handle_getcareplan_test.go
@@ -5,10 +5,14 @@ import (
 	"errors"
 	fhirclient "github.com/SanteonNL/go-fhir-client"
 	"github.com/SanteonNL/orca/orchestrator/careplancontributor/mock"
+	"github.com/SanteonNL/orca/orchestrator/lib/auth"
 	"github.com/SanteonNL/orca/orchestrator/lib/to"
 	"github.com/stretchr/testify/require"
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 	"go.uber.org/mock/gomock"
+	"net/url"
+	"os"
+	"reflect"
 	"testing"
 )
 
