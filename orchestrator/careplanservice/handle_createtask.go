90e901057c3a01d77c4a79aacb5f11b3f4561647
Rein Krul <info@reinkrul.nl>
2024-11-19 16:08:54 +0100

    fix

--- orchestrator/careplanservice/handle_createtask.go
+++ orchestrator/careplanservice/handle_createtask.go
@@ -108,19 +108,20 @@ func (s *Service) handleCreateTask(ctx context.Context, request FHIRHandlerReque
 		if task.For == nil || !coolfhir.ValidateReference(*task.For) {
 			return nil, coolfhir.NewErrorWithCode(fmt.Sprintf("Task.For must be set with a local reference, or a logical identifier, referencing a patient"), http.StatusBadRequest)
 		}
-		// TODO: INT-450 - Re-enable task.For dereferencing logical identifiers to actual patient, when Frontend is fixed.
-		//if task.For.Reference == nil {
-		//	headers := fhirclient.Headers{}
-		//	patients, _, err := handleSearchResource[fhir.Patient](s, "Patient", map[string][]string{"identifier": {fmt.Sprintf("%s|%s", *task.For.Identifier.System, *task.For.Identifier.Value)}}, &headers)
-		//	if err != nil {
-		//		return nil, err
-		//	}
-		//	if len(patients) == 0 {
-		//		return nil, coolfhir.NewErrorWithCode("Task.For must be set with a local reference, or a logical identifier, referencing an existing patient", http.StatusNotFound)
-		//	}
-		//
-		//	task.For.Reference = to.Ptr("Patient/" + *patients[0].Id)
-		//}
+		if task.For.Reference == nil {
+			headers := fhirclient.Headers{}
+			patients, _, err := handleSearchResource[fhir.Patient](s, "Patient", map[string][]string{"identifier": {fmt.Sprintf("%s|%s", *task.For.Identifier.System, *task.For.Identifier.Value)}}, &headers)
+			if err != nil {
+				return nil, err
+			}
+
+			if len(patients) == 0 {
+				// TODO: INT-450 - Re-enable task.For dereferencing logical identifiers to actual patient, when Frontend is fixed.
+				//return nil, coolfhir.NewErrorWithCode("Task.For must be set with a local reference, or a logical identifier, referencing an existing patient", http.StatusNotFound)
+			} else {
+				task.For.Reference = to.Ptr("Patient/" + *patients[0].Id)
+			}
+		}
 		carePlan.Subject = *task.For
 		carePlan.Status = fhir.RequestStatusActive
 
