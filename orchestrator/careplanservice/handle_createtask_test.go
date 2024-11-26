aa1ec02fcb65cf6e141472733a14349d43202f44
Rein Krul <info@reinkrul.nl>
2024-11-26 13:30:23 +0100

    INT-450: Re-enable patient existence check

--- orchestrator/careplanservice/handle_createtask_test.go
+++ orchestrator/careplanservice/handle_createtask_test.go
@@ -258,12 +258,11 @@ func Test_handleCreateTask_NoExistingCarePlan(t *testing.T) {
 				}
 			}),
 		},
-		// TODO: INT-450 - Re-enable task.For dereferencing logical identifiers to actual patient, when Frontend is fixed.
-		//{
-		//	name:                       "error: Task.for contains a logical identifier with BSN, search for patient fails",
-		//	errorFromPatientBundleRead: errors.New("fhir error: Issues searching for patient"),
-		//	expectError:                errors.New("fhir error: Issues searching for patient"),
-		//},
+		{
+			name:                       "error: Task.for contains a logical identifier with BSN, search for patient fails",
+			errorFromPatientBundleRead: errors.New("fhir error: Issues searching for patient"),
+			expectError:                errors.New("fhir error: Issues searching for patient"),
+		},
 		{
 			name: "Task.for contains a local reference and a logical identifier with BSN",
 			taskToCreate: deep.AlterCopy(defaultTask, func(task *fhir.Task) {
