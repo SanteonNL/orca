e7c6cb85367740fb8e907be297668f954080353d
reinkrul <info@reinkrul.nl>
2024-12-16 13:57:58 +0100

    INT-508: Fix and cleanup local dev deployment (#237)

--- deployments/dev/update.sh
+++ deployments/dev/update.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 set -e
 
-# read the tunnel URL from the tunnel log file. It takes the party name (clinic/hospital).
-function readTunnelURL() {
-  local tunnelLogFile=$1/data/tunnel.log
-  TUNNEL_URL=$(grep "Connect via browser:" $tunnelLogFile | awk '{print $4}')
-  echo $TUNNEL_URL
-}
-
-CLINIC_URL=$(readTunnelURL ./clinic)
 pushd clinic
-docker compose pull
-NUTS_URL="${CLINIC_URL}" \
-  docker compose up \
- --wait --build --remove-orphans
- popd
+docker compose --env-file .env pull
+docker compose --env-file .env up --wait --build --remove-orphans
+popd
 
-HOSPITAL_URL=$(readTunnelURL ./hospital)
-CAREPLANCONTRIBUTOR_CAREPLANSERVICE_URL="${HOSPITAL_URL}/orca/cps"
 pushd hospital
-docker compose pull
-NUTS_URL="${HOSPITAL_URL}" \
- CAREPLANCONTRIBUTOR_CAREPLANSERVICE_URL="${CAREPLANCONTRIBUTOR_CAREPLANSERVICE_URL}" \
-  docker compose up \
- --wait --build --remove-orphans
+docker compose --env-file .env pull
+docker compose --env-file .env up --wait --build --remove-orphans
 popd
