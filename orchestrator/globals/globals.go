634a563c5fa5f0a9c6673490c18df39df8becda7
reinkrul <info@reinkrul.nl>
2025-01-09 13:20:07 +0100

    INT-527: only map test BSN in strict mode, add graceful shutdown (#280)

--- orchestrator/globals/globals.go
+++ orchestrator/globals/globals.go
@@ -3,3 +3,7 @@ package globals
 import fhirclient "github.com/SanteonNL/go-fhir-client"
 
 var CarePlanServiceFhirClient fhirclient.Client
+
+// StrictMode is a global variable that can be set to true to enable strict mode. If strict mode is enabled,
+// potentially unsafe behavior is disabled.
+var StrictMode bool
