cac201a5f05ab6924f66ea94621b35629d139f49
Joris Scharp <joris@headease.nl>
2025-01-13 13:56:31 +0100

    INT-473: Enhance EnrollmentTaskPage to display task status messages based on the Task.status.

--- frontend/app/enrollment/task/[taskId]/page.tsx
+++ frontend/app/enrollment/task/[taskId]/page.tsx
@@ -4,10 +4,11 @@ import useTaskProgressStore from '@/lib/store/task-progress-store'
 import { useParams } from 'next/navigation'
 import Loading from '@/app/enrollment/loading'
 import QuestionnaireRenderer from '../../components/questionnaire-renderer'
+import organizationName from '@/lib/fhirUtils'
 
 export default function EnrollmentTaskPage() {
     const { taskId } = useParams()
-    const { loading, initialized, setSelectedTaskId, subTasks, taskToQuestionnaireMap } = useTaskProgressStore()
+    const { task, loading, initialized, setSelectedTaskId, subTasks, taskToQuestionnaireMap } = useTaskProgressStore()
 
     useEffect(() => {
         if (taskId) {
@@ -19,14 +20,54 @@ export default function EnrollmentTaskPage() {
 
     if (loading || !initialized) return <Loading />
 
-    if (!subTasks || !subTasks.length || !taskToQuestionnaireMap || !subTasks?.[0].id || !taskToQuestionnaireMap[subTasks[0].id]) {
+    if (!task || !subTasks || !subTasks.length || !taskToQuestionnaireMap || !subTasks?.[0].id || !taskToQuestionnaireMap[subTasks[0].id]) {
         return <></>
     }
 
-    return (
-        <QuestionnaireRenderer
-            questionnaire={taskToQuestionnaireMap[subTasks[0].id]}
-            inputTask={subTasks[0]}
-        />
-    )
+    const serviceName = task.focus?.display || "Unknown"
+    const conditionName = task.reasonCode?.coding?.[0].display || "Unknown"
+    const taskDate = task.meta?.lastUpdated ? new Date(task.meta.lastUpdated).toLocaleDateString("nl-NL") : "Onbekend"
+
+    switch (task.status) {
+        case "accepted":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren, is door {organizationName(task.owner)} op {taskDate} geaccepteerd. Uitvoering is nog niet gestart.
+            </div>
+        case "completed":
+            return <div className='w-[568px]'>
+                De uitvoering van {serviceName} voor {conditionName} is door {organizationName(task.owner)} afgerond op {taskDate}.
+            </div>
+        case "cancelled":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren, is op {taskDate} geannuleerd door {organizationName(task.owner)}.
+            </div>
+        case "failed":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren, is sinds {taskDate} gemarkeerd als &quot;mislukt&quot; door {organizationName(task.owner)}.
+            </div>
+        case "in-progress":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren, wordt momenteel (sinds {taskDate}) uitgevoerd door {organizationName(task.owner)}.
+            </div>
+        case "on-hold":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren, is sinds {taskDate} gepauseerd door {organizationName(task.owner)}.
+            </div>
+        case "requested":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} is verstuurd naar {organizationName(task.owner)}, maar nog niet ontvangen.
+            </div>
+        case "received":
+            return <QuestionnaireRenderer
+                questionnaire={taskToQuestionnaireMap[subTasks[0].id]}
+                inputTask={subTasks[0]}
+            />
+        case "rejected":
+            return <div className='w-[568px]'>
+                Het verzoek om {serviceName} voor {conditionName} uit te voeren is op {taskDate} afgewezen door {organizationName(task.owner)}.
+            </div>
+        default:
+            //primary tasks cannot handle Task.stats, for example `ready`
+            return <p>Task status {task.status} is geen valide status voor een enrollment Task.</p>
+    }
 }
