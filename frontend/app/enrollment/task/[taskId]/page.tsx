a73a040b53804462c7645f24a6590811d75939fd
Joris Scharp <joris@headease.nl>
2025-01-13 17:16:53 +0100

    INT-473: Enhance EnrollmentTaskPage to handle optional chaining for task properties and improve status message for 'received' state.

--- frontend/app/enrollment/task/[taskId]/page.tsx
+++ frontend/app/enrollment/task/[taskId]/page.tsx
@@ -20,15 +20,11 @@ export default function EnrollmentTaskPage() {
 
     if (loading || !initialized) return <Loading />
 
-    if (!task || !subTasks || !subTasks.length || !taskToQuestionnaireMap || !subTasks?.[0].id || !taskToQuestionnaireMap[subTasks[0].id]) {
-        return <></>
-    }
+    const serviceName = task?.focus?.display || "Unknown"
+    const conditionName = task?.reasonCode?.coding?.[0].display || "Unknown"
+    const taskDate = task?.meta?.lastUpdated ? new Date(task.meta.lastUpdated).toLocaleDateString("nl-NL") : "Onbekend"
 
-    const serviceName = task.focus?.display || "Unknown"
-    const conditionName = task.reasonCode?.coding?.[0].display || "Unknown"
-    const taskDate = task.meta?.lastUpdated ? new Date(task.meta.lastUpdated).toLocaleDateString("nl-NL") : "Onbekend"
-
-    switch (task.status) {
+    switch (task?.status) {
         case "accepted":
             return <div className='w-[568px]'>
                 Het verzoek om {serviceName} voor {conditionName} uit te voeren, is door {organizationName(task.owner)} op {taskDate} geaccepteerd. Uitvoering is nog niet gestart.
@@ -58,6 +54,9 @@ export default function EnrollmentTaskPage() {
                 Het verzoek om {serviceName} voor {conditionName} is verstuurd naar {organizationName(task.owner)}, maar nog niet ontvangen.
             </div>
         case "received":
+            if (!taskToQuestionnaireMap || !subTasks?.[0]?.id || !taskToQuestionnaireMap[subTasks[0].id]) {
+                return <>Task is ontvangen, maar er ontbreekt informatie</>
+            }
             return <QuestionnaireRenderer
                 questionnaire={taskToQuestionnaireMap[subTasks[0].id]}
                 inputTask={subTasks[0]}
@@ -68,6 +67,6 @@ export default function EnrollmentTaskPage() {
             </div>
         default:
             //primary tasks cannot handle Task.stats, for example `ready`
-            return <p>Task status {task.status} is geen valide status voor een enrollment Task.</p>
+            return <p>Task status {task?.status || "Taak niet gevonden"} is geen valide status voor een enrollment Task.</p>
     }
 }
