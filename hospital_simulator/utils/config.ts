54fcbafc61dabdce95d92305dd662dbfc49e443f
Rein Krul <info@reinkrul.nl>
2025-01-20 13:31:59 +0100

    Hospital demo app: pass identifier value only, not system

--- hospital_simulator/utils/config.ts
+++ hospital_simulator/utils/config.ts
@@ -18,7 +18,7 @@ export const getEnrollmentUrl = async (patientId: string, serviceRequestId: stri
             serviceRequest: `ServiceRequest/${serviceRequestId}`,
             practitioner: `Practitioner/${respBundle.entry[0].resource?.id}`, //TODO: Rework to get reference from ServiceRequest.requester - currently an Organization, but should be a PractitionerRole
             iss: `${process.env.FHIR_BASE_URL}`,
-            taskIdentifier: `http://demo-launch/fhir/NamingSystem/task-identifier|${serviceRequestId}`
+            taskIdentifier: serviceRequestId
         }).toString()
     }
 
