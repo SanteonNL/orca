5ef3d717a03a3c7865c038ad54b979cb0a357c54
Joris Scharp <joris@headease.nl>
2025-01-28 16:49:38 +0100

    INT-524: Fixed horizontal scrollbar. Was only able to reproduce this on a windows machine.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <main className="h-screen w-screen">
+        <main>
           {children}
           <Toaster />
         </main>
