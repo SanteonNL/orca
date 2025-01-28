b5b1b8b14af047b4cfb38edbd37982d897f9dad6
David Welman <dwelman@xebia.com>
2025-01-28 09:47:51 +0100

    INT-559 | Prevent duplicate Task enrollment (#326)
    * INT-559 | Prevent duplicate Task enrollment
    
    Re-enable IfNoneExists headers to ensure enrollment is persisted for the same task per ServiceRequest, prevents duplicate ServiceRequests and Tasks from being created
    
    * Remove FHIR type from query
    
    * Use task identifier for ServiceRequest

--- frontend/lib/fhirUtils.ts
+++ frontend/lib/fhirUtils.ts
@@ -94,7 +94,7 @@ const cleanPatient = (patient: Patient) => {
     return cleanedPatient;
 }
 
-const cleanServiceRequest = (serviceRequest: ServiceRequest, patient: Patient, patientReference: string) => {
+const cleanServiceRequest = (serviceRequest: ServiceRequest, patient: Patient, patientReference: string, taskIdentifier?: string) => {
     // Clean up the ServiceRequest by removing relative references - the CPS won't understand them
     const cleanedServiceRequest = {...serviceRequest, id: undefined};
 
@@ -121,6 +121,10 @@ const cleanServiceRequest = (serviceRequest: ServiceRequest, patient: Patient, p
         if (item?.reference) {
             delete item.reference;
         }
+    }
+
+    if (taskIdentifier) {
+        cleanedServiceRequest.identifier = parseTaskIdentifier(taskIdentifier);
     }
 
     return cleanedServiceRequest;
@@ -160,17 +164,20 @@ export const constructBundleTask = (serviceRequest: ServiceRequest, primaryCondi
     } as Task
 
     if (taskIdentifier) {
-        // TODO: Fix ifNoneExists
-        // const systemAndIdentifier = taskIdentifier.split("|")
-        // if (systemAndIdentifier.length !== 2) throw new Error("Invalid task identifier - expecting `system|identifier`")
-        // task.identifier = [{
-        //     system: systemAndIdentifier[0],
-        //     value: systemAndIdentifier[1],
-        // }]
+        task.identifier = parseTaskIdentifier(taskIdentifier);
     }
 
     return task
 }
+
+const parseTaskIdentifier = (taskIdentifier: string) => {
+    const systemAndIdentifier = taskIdentifier.split("|");
+    if (systemAndIdentifier.length !== 2) throw new Error("Invalid task identifier - expecting `system|identifier`");
+    return [{
+        system: systemAndIdentifier[0],
+        value: systemAndIdentifier[1],
+    }];
+};
 
 export const constructTaskBundle = (serviceRequest: ServiceRequest, primaryCondition: Condition, patient: Patient, taskIdentifier?: string): Bundle & {
     type: "transaction"
@@ -178,7 +185,7 @@ export const constructTaskBundle = (serviceRequest: ServiceRequest, primaryCondi
     const cleanedPatient = cleanPatient(patient);
     const serviceRequestEntry = {
         fullUrl: "urn:uuid:serviceRequest",
-        resource: cleanServiceRequest(serviceRequest, patient, "urn:uuid:patient"),
+        resource: cleanServiceRequest(serviceRequest, patient, "urn:uuid:patient", taskIdentifier),
         request: {
             method: "POST",
             url: "ServiceRequest",
@@ -195,9 +202,8 @@ export const constructTaskBundle = (serviceRequest: ServiceRequest, primaryCondi
         }
     }
     if (taskIdentifier) {
-        // TODO: Fix ifNoneExists
-        // serviceRequestEntry.request.ifNoneExist = `identifier=${taskIdentifier}`
-        // taskEntry.request.ifNoneExist = `identifier=${taskIdentifier}`
+        serviceRequestEntry.request.ifNoneExist = `identifier=${taskIdentifier}`
+        taskEntry.request.ifNoneExist = `identifier=${taskIdentifier}`
     }
     const bundle = {
         resourceType: "Bundle",
@@ -209,8 +215,7 @@ export const constructTaskBundle = (serviceRequest: ServiceRequest, primaryCondi
                 request: {
                     method: "POST",
                     url: "Patient",
-                    // TODO: Fix ifNoneExists
-                    // ifNoneExist: `identifier=http://fhir.nl/fhir/NamingSystem/bsn|${getBsn(patient)}`
+                    ifNoneExist: `identifier=http://fhir.nl/fhir/NamingSystem/bsn|${getBsn(patient)}`
                 }
             },
             serviceRequestEntry,
