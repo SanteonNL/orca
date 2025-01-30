9a2f8cb773029b798d0733911eb51ae29cf5e0bd
David Welman <dwelman@xebia.com>
2025-01-30 17:51:02 +0100

    Temporarily re-implement viewer searching for task and careplan (#356)
    The notification mechanism is not working correctly in dev and test, re-enable fetching Task and CarePlan if there are no records so we can still do tests and demos while figuring out the notifications

--- viewer_simulator/app/(DashboardLayout)/components/bgz/bgz-overview.tsx
+++ viewer_simulator/app/(DashboardLayout)/components/bgz/bgz-overview.tsx
@@ -22,6 +22,35 @@ export default async function BgzOverview() {
         const notificationBundles = getNotificationBundles();
         let entries = notificationBundles.flatMap(bundle => bundle.entry || []);
 
+        // TODO: Remove this once the notification from ehr to viewer is working correctly in dev and test environments
+        if (entries.length === 0) {
+            let requestHeaders = new Headers();
+            requestHeaders.set("Cache-Control", "no-cache")
+            if (process.env.FHIR_AUTHORIZATION_TOKEN) {
+                requestHeaders.set("Authorization", "Bearer " + process.env.FHIR_AUTHORIZATION_TOKEN);
+            }
+            requestHeaders.set("Content-Type", "application/x-www-form-urlencoded");
+            const response = await fetch(`${process.env.FHIR_BASE_URL}/CarePlan/_search`, {
+                method: 'POST',
+                headers: requestHeaders,
+                body: new URLSearchParams({
+                    '_sort': '-_lastUpdated',
+                    '_count': '100',
+                    '_include': 'CarePlan:care-team'
+                })
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.error('Failed to fetch tasks: ', errorText);
+                throw new Error('Failed to fetch tasks: ' + errorText);
+            }
+
+            const responseBundle = await response.json() as Bundle;
+            entries = responseBundle.entry || [];
+            console.log(`Found [${entries?.length}] CarePlan resources`);
+        }
+
         //map all the resources to their reference as it contains CarePlans, Patients, Tasks and CareTeams
         const resourceMap = entries?.reduce((map, entry: BundleEntry) => {
             const resource = entry.resource;
