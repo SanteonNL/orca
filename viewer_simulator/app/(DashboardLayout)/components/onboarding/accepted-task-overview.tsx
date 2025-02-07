670010d9499a509ad9dd802a4804dc862f06445b
david-welman <dwelman@xebia.com>
2025-02-07 14:32:56 +0100

    Remove fallback in viewer that fetches when task/careplan is not set locally

--- viewer_simulator/app/(DashboardLayout)/components/onboarding/accepted-task-overview.tsx
+++ viewer_simulator/app/(DashboardLayout)/components/onboarding/accepted-task-overview.tsx
@@ -22,35 +22,6 @@ export default async function AcceptedTaskOverview() {
         let entries = notificationBundles.flatMap(bundle => bundle.entry || []);
         console.log(`Found [${entries?.length}] bundle resources`);
 
-        // TODO: Remove this once the notification from ehr to viewer is working correctly in dev and test environments
-        if (entries.length === 0) {
-            let requestHeaders = new Headers();
-            requestHeaders.set("Cache-Control", "no-cache")
-            if (process.env.FHIR_AUTHORIZATION_TOKEN) {
-                requestHeaders.set("Authorization", "Bearer " + process.env.FHIR_AUTHORIZATION_TOKEN);
-            }
-            requestHeaders.set("Content-Type", "application/x-www-form-urlencoded");
-            const response = await fetch(`${process.env.FHIR_BASE_URL}/Task/_search`, {
-                method: 'POST',
-                cache: 'no-store',
-                headers: requestHeaders,
-                body: new URLSearchParams({
-                    '_sort': '-_lastUpdated',
-                    '_count': '100'
-                })
-            });
-
-            if (!response.ok) {
-                const errorText = await response.text();
-                console.error('Failed to fetch tasks: ', errorText);
-                throw new Error('Failed to fetch tasks: ' + errorText);
-            }
-
-            const responseBundle = await response.json() as Bundle;
-            entries = responseBundle.entry || [];
-            console.log(`Found [${entries?.length}] Task resources`);
-        }
-
         if (entries?.length) {
             rows = entries.
             filter((entries) => entries.resource?.resourceType === "Task").
