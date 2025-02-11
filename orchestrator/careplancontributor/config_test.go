4214b9b0d771e3413fe6440c5121a2a700007646
david-welman <dwelman@xebia.com>
2025-02-11 11:12:30 +0100

    Remove validation requirement for CPS URL

--- orchestrator/careplancontributor/config_test.go
+++ orchestrator/careplancontributor/config_test.go
@@ -11,12 +11,6 @@ func TestConfig_Validate(t *testing.T) {
 		err := Config{}.Validate()
 		require.NoError(t, err)
 	})
-	t.Run("missing care plan service URL", func(t *testing.T) {
-		err := Config{
-			Enabled: true,
-		}.Validate()
-		require.EqualError(t, err, "careplancontributor.careplanservice.url is not configured")
-	})
 }
 
 func TestDefaultConfig(t *testing.T) {
