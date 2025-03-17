dad679713b873d6b70068258db5c50c24858d2e0
Dillen Meijboom <dillen@brainhive.nl>
2025-03-17 21:33:31 +0100

    feat: make demo viewer multi-tenant

--- viewer_simulator/app/(DashboardLayout)/bgz/page.tsx
+++ viewer_simulator/app/(DashboardLayout)/bgz/page.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import PageContainer from '../components/container/PageContainer'
-import { Box, Typography } from '@mui/material'
-import DashboardCard from '../components/shared/DashboardCard'
-import BgzOverview from '../components/bgz/bgz-overview'
+'use client';
+
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+    Box,
+    Typography,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+} from '@mui/material';
+
+import PageContainer from '../components/container/PageContainer';
+import DashboardCard from '../components/shared/DashboardCard';
+import BgzOverview from '../components/bgz/bgz-overview';
+
+import useFetch from '@/app/useFetch';
 
 export default function CarePlans() {
+    const [name, setName] = useState<string | null>(null);
+    const { data: endpoints } =
+        useFetch<{ name: string; endpoint: string }[]>(`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/api/bgz/endpoints`);
+    const updateEndpoint = useCallback((e: SelectChangeEvent<string>) => {
+        setName(e.target.value);
+    }, []);
+
+    useEffect(() => {
+        if (!name && endpoints) {
+            setName(endpoints[0].name);
+        }
+    }, [name, endpoints]);
 
     return (
         <Box sx={{ position: 'relative' }}>
@@ -14,11 +37,32 @@ export default function CarePlans() {
             >
                 <DashboardCard title="Care Plans">
                     <>
-                        <Typography sx={{ mb: 2 }}>Use the CarePlanContributer to gather all health care data known about a patient. All organizations that are a member ofthe CareTeam will be queried.</Typography>
-                        <BgzOverview />
+                        <Typography sx={{ mb: 2 }}>
+                            Use the CarePlanContributer to gather all health
+                            care data known about a patient. All organizations
+                            that are a member ofthe CareTeam will be queried.
+                        </Typography>
+                        {(endpoints?.length || 0) > 1 && (
+                            <Select
+                                label="Endpoint"
+                                value={name || ''}
+                                sx={{ mb: 2 }}
+                                onChange={updateEndpoint}
+                            >
+                                {endpoints?.map((endpoint) => (
+                                    <MenuItem
+                                        key={endpoint.name}
+                                        value={endpoint.name}
+                                    >
+                                        {endpoint.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        )}
+                        {name && <BgzOverview name={name} />}
                     </>
                 </DashboardCard>
             </PageContainer>
         </Box>
-    )
+    );
 }
--- viewer_simulator/app/(DashboardLayout)/components/bgz/bgz-overview.tsx
+++ viewer_simulator/app/(DashboardLayout)/components/bgz/bgz-overview.tsx
@@ -1,73 +1,16 @@
 import React from 'react';
-import { BundleEntry, CarePlan } from 'fhir/r4';
-import CarePlanTable from './bgz-careplan-table';
-import { getBsn } from '@/utils/fhirUtils';
-import { headers } from 'next/headers'
-import { getNotificationBundles } from '@/app/api/delivery/orca-enroll-patient/storage';
 
-export default async function BgzOverview() {
+import useFetch from '@/app/useFetch';
+import CarePlanTable, { Row } from './bgz-careplan-table';
 
-    if (!process.env.FHIR_BASE_URL) {
-        console.error('FHIR_BASE_URL is not defined');
-        return <>FHIR_BASE_URL is not defined</>;
-    }
-
-    // prevent ssr from pre-rendering the page, as it won't be able to fetch resources from process.env.FHIR_BASE_URL
-    const headersList = headers()
-
-    let rows: any[] = [];
-
-    try {
-        // Get bundles from internal storage, join all entries
-        const notificationBundles = await getNotificationBundles();
-        let entries = notificationBundles.flatMap(bundle => bundle.entry || []);
-
-        //map all the resources to their reference as it contains CarePlans, Patients, Tasks and CareTeams
-        const resourceMap = entries?.reduce((map, entry: BundleEntry) => {
-            const resource = entry.resource;
-            map.set(`${resource?.resourceType}/${resource?.id}`, resource);
-            return map;
-        }, new Map<string, any>());
-
-
-        rows = entries?.filter((entries) => entries.resource?.resourceType === "CarePlan")
-            .map((entry: any) => entry.resource as CarePlan)
-            .map((carePlan: CarePlan) => {
-
-                // Find the careteam, first as a contained resource, otherwise as a referenced resource that has been notified
-                const careTeam =
-                    carePlan.contained?.find((resource: any) => resource.resourceType === "CareTeam") ??
-                    (carePlan.careTeam?.[0]?.reference ? resourceMap?.get(carePlan.careTeam[0].reference) : undefined);
-
-                if (!careTeam) {
-                    console.warn(`No CareTeam found for CarePlan/${carePlan.id}`);
-                }
-
-                return {
-                    id: careTeam?.id || "Unknown",
-                    bsn: getBsn(carePlan),
-                    category: carePlan.category?.[0]?.coding?.map(coding => coding.display).join(', ') ?? "Unknown",
-                    carePlan,
-                    careTeam,
-                    status: carePlan.status,
-                    lastUpdated: carePlan.meta?.lastUpdated ? new Date(carePlan.meta.lastUpdated) : new Date(),
-                    careTeamMembers: careTeam?.participant?.map((participant: any) => {
-                        const display = participant.member.display ? participant.member.display + ' ' : '';
-                        const ura = `(URA ${participant.member.identifier?.value || 'Unknown'})`;
-                        return display + ura;
-                    }).join(', ') ?? "Unknown"
-                };
-            }) || [];
-    } catch (error) {
-        if (error instanceof Error && 'digest' in error && error.digest === 'DYNAMIC_SERVER_USAGE') {
-            console.error('Error occurred while fetching tasks:', error);
-        } else {
-
-            throw error;
-        }
-    }
+export default function BgzOverview({ name }: { name: string }) {
+    const { loading, data: rows } = useFetch<Row[]>(
+        `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/api/bgz/careplans?name=${encodeURIComponent(name)}`,
+    );
 
     return (
-        <CarePlanTable rows={rows} />
+        <div>
+            <CarePlanTable name={name} rows={rows || []} loading={loading} />
+        </div>
     );
 }
