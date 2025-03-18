50954737abdadfb79675044f364e6702a4e7ea98
Rein Krul <info@reinkrul.nl>
2025-03-18 06:35:45 +0100

    Remove test

--- orchestrator/careplancontributor/integration_test.go
+++ orchestrator/careplancontributor/integration_test.go
@@ -176,32 +176,6 @@ func Test_Integration_CPCFHIRProxy(t *testing.T) {
 		require.Error(t, err)
 		require.Len(t, fetchedBundle.Entry, 0)
 	}
-	t.Log("CPC client without CPS URL, returns error")
-	{
-		cpsProxy := coolfhir.NewProxy("CPS->CPC", fhirBaseURL, "/cpc/fhir", orcaPublicURL, httpService.Client().Transport, true, false)
-
-		messageBroker, err := messaging.New(messaging.Config{}, nil)
-		require.NoError(t, err)
-
-		cpcConfig := DefaultConfig()
-		cpcConfig.Enabled = true
-		cpcConfig.FHIR.BaseURL = fhirBaseURL.String()
-		cpcConfig.HealthDataViewEndpointEnabled = true
-		cpc, err := New(cpcConfig, profile.TestProfile{}, orcaPublicURL, sessionManager, messageBroker, cpsProxy, nil)
-		require.NoError(t, err)
-
-		cpcServerMux := http.NewServeMux()
-		cpcHttpService := httptest.NewServer(cpcServerMux)
-		cpc.RegisterHandlers(cpcServerMux)
-		cpcURL, _ := url.Parse(cpcHttpService.URL + "/cpc/fhir")
-
-		cpcDataRequester := fhirclient.New(cpcURL, &http.Client{Transport: auth.AuthenticatedTestRoundTripper(httpService.Client().Transport, auth.TestPrincipal1, carePlanServiceURL.String()+"/CarePlan/"+*carePlan.Id)}, nil)
-
-		var fetchedTask fhir.Task
-
-		err = cpcDataRequester.Read("Task/"+*task.Id, &fetchedTask)
-		require.ErrorContains(t, err, "This ORCA instance has no local CarePlanService, API can't be used.")
-	}
 }
 
 func setupIntegrationTest(t *testing.T, notificationEndpoint *url.URL) (*url.URL, *httptest.Server, *url.URL) {
