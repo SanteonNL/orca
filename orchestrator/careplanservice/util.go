eead9396f90b961906d7364ac934aba8fadd986a
Rein Krul <info@reinkrul.nl>
2025-04-24 15:11:37 +0200

    cleanup

--- orchestrator/careplanservice/util.go
+++ orchestrator/careplanservice/util.go
@@ -13,43 +13,6 @@ import (
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 )
 
-func (s *Service) isCreatorOfResource(ctx context.Context, principal auth.Principal, resourceType string, resourceID string) (bool, error) {
-
-	// TODO: Find a more suitable way to handle this auth.
-	// The AuditEvent implementation has proven unsuitable and we are using the AuditEvent for unintended purposes.
-	// For now, we can return true, as this will follow the same logic as was present before implementing the AuditEvent.
-
-	return true, nil
-
-	//var auditBundle fhir.Bundle
-	//err := s.fhirClient.SearchWithContext(ctx, "AuditEvent", url.Values{
-	//	"entity": []string{resourceType + "/" + resourceID},
-	//	"action": []string{fhir.AuditEventActionC.String()},
-	//}, &auditBundle)
-	//if err != nil {
-	//	return false, fmt.Errorf("failed to find creation AuditEvent: %w", err)
-	//}
-	//
-	//// Check if there's a creation audit event
-	//if len(auditBundle.Entry) == 0 {
-	//	return false, coolfhir.NewErrorWithCode(fmt.Sprintf("No creation audit event found for %s", resourceType), http.StatusForbidden)
-	//}
-	//
-	//// Get the creator from the audit event
-	//var creationAuditEvent fhir.AuditEvent
-	//err = json.Unmarshal(auditBundle.Entry[0].Resource, &creationAuditEvent)
-	//if err != nil {
-	//	return false, fmt.Errorf("failed to unmarshal AuditEvent: %w", err)
-	//}
-	//
-	//// Check if the current user is the creator
-	//if !audit.IsCreator(creationAuditEvent, &principal) {
-	//	return false, coolfhir.NewErrorWithCode(fmt.Sprintf("Only the creator can update this %s", resourceType), http.StatusForbidden)
-	//}
-	//
-	//return true, nil
-}
-
 // handleSearchResource is a generic function to search for a resource of a given type and return the results
 // it returns a processed list of the required resource type, the full bundle and an error
 func handleSearchResource[T any](ctx context.Context, s *Service, resourceType string, queryParams url.Values, headers *fhirclient.Headers) ([]T, *fhir.Bundle, error) {
