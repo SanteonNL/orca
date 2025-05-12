3f83cbec151148a1e8c4822c92611b6afecfb58f
David Welman <dwelman@xebia.com>
2025-05-12 11:30:00 +0200

    DEV-145 | Implement resource-creator based authz (#604)
    * DEV-145 | Implement resource-creator based authz
    
    When a resource is created, append the Extension field with the creating organisation.
    
    This can be used for authz when trying to read or update the resource.
    
    Previously this was attempted with FHIR AuditEvent and subsequently disabled due to unsuitability, this change reimplements it with the new authz code.
    
    * merge decision log changes, remove old commented code
    
    * Refactor authz to use fhir.HasExtension base type
    
    * Append resourceCreator extension rather than overwrite
    
    * Dedupe code and add unit test

--- orchestrator/careplanservice/authz_careplan.go
+++ orchestrator/careplanservice/authz_careplan.go
@@ -2,6 +2,6 @@ package careplanservice
 
 import "github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 
-func ReadCarePlanAuthzPolicy() Policy[fhir.CarePlan] {
+func ReadCarePlanAuthzPolicy() Policy[*fhir.CarePlan] {
 	return CareTeamMemberPolicy[fhir.CarePlan]{}
 }
