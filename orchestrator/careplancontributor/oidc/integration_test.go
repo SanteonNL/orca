bd10d4661371260fe0583c3e20105fdfdaefbd99
Rein Krul <info@reinkrul.nl>
2025-05-12 13:38:59 +0200

    f

--- orchestrator/careplancontributor/oidc/integration_test.go
+++ orchestrator/careplancontributor/oidc/integration_test.go
@@ -127,5 +127,4 @@ func TestService_IntegrationTest(t *testing.T) {
 	assert.Equal(t, "john@example.com", capturedIDTokenClaims.GetUserInfo().Email)
 	assert.Equal(t, []any{"nurse-level-4@example.com/CodeSystem"}, capturedIDTokenClaims.Claims["roles"])
 	assert.Equal(t, []any{"example.com/CodeSystem|SOME-PATIENT-IDENTIFIER"}, capturedIDTokenClaims.Claims["patient"])
-	assert.Equal(t, []any{"example.com/CodeSystem|SOME-ORG-IDENTIFIER"}, capturedIDTokenClaims.Claims["orgid"])
 }
