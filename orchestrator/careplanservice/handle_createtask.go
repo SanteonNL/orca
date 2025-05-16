edc4ef5fcd001b99ab09c50294fd3277d4d9bbfe
reinkrul <info@reinkrul.nl>
2025-05-16 12:18:58 +0200

    CPS: Use Task.Requester as CareTeam member, so displayName is set (#633)

--- orchestrator/careplanservice/handle_createtask.go
+++ orchestrator/careplanservice/handle_createtask.go
@@ -133,10 +133,7 @@ func (s *Service) handleCreateTask(ctx context.Context, request FHIRHandlerReque
 			}
 		}
 
-		ok := careteamservice.ActivateMembership(&careTeam, &fhir.Reference{
-			Identifier: &request.Principal.Organization.Identifier[0],
-			Type:       to.Ptr("Organization"),
-		})
+		ok := careteamservice.ActivateMembership(&careTeam, task.Requester)
 		if !ok {
 			return nil, errors.New("failed to activate membership for new CareTeam")
 		}
