3f7e8151b6fb0606260895973a1575947034e72d
reinkrul <info@reinkrul.nl>
2025-05-20 10:25:29 +0200

    DEV-231: Remove CPC's /aggregate endpoint, use new /external endpoint instead (#629)

---
+++ viewer_simulator/utils/config.ts
@@ -0,0 +1,48 @@
+import {Identifier} from "fhir/r4"
+import {trimEnd} from "lodash";
+
+export function getOwnIdentifier(name: string): Identifier {
+    const ownIdentifier = `${process.env[`${name}_IDENTIFIER`]}`
+    if (!ownIdentifier) {
+        throw new Error(`Own identifier not configured ('${name}_IDENTIFIER', in the format of system|value)`);
+    }
+    const [system, value] = ownIdentifier.split('|');
+    if (!system || !value) {
+        throw new Error(`Own identifier is invalid ('${name}_IDENTIFIER', in the format of system|value)`);
+    }
+    return {
+        system: system,
+        value: value,
+    }
+}
+
+export function getORCABaseURL(name: string): string {
+    const orcaBaseURL = `${process.env[`${name}_ORCA_URL`]}`;
+    if (!orcaBaseURL) {
+        throw new Error(`ORCA base URL not configured ('${name}_ORCA_URL')`);
+    }
+    return orcaBaseURL;
+}
+
+// getORCAExternalFHIRBaseURL returns the FHIR base URL for querying remote SCP-nodes' FHIR endpoints.
+export function getORCAExternalFHIRBaseURL(name: string): string {
+    const orcaBaseURL = getORCABaseURL(name);
+    return `${trimEnd(orcaBaseURL, '/')}/cpc/external/fhir`
+}
+
+export function getORCABearerToken(name: string): string {
+    const bearerToken = `${process.env[`${name}_ORCA_BEARERTOKEN`]}`
+    if (!bearerToken) {
+        throw new Error(`Bearer token to call own ORCA not configured ('${name}_ORCA_BEARERTOKEN')`);
+    }
+    return bearerToken;
+}
+
+export function getCarePlanServiceBaseURLs(name: string): string[] {
+    const val = `${process.env[`${name}_CPS_URL`]}`;
+    if (!val) {
+        throw new Error(`Remote CarePlanService URL(s) not configured ('${name}_CPS_URL')`);
+    }
+    // split on , and trim spaces
+    return val.split(',').map((url) => url.trim());
+}
