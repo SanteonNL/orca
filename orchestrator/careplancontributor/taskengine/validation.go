b324e757252a7f28fda75becff53991b39a1dcdf
Jethro Sloan <jdksloan@gmail.com>
2025-06-17 16:44:48 +0200

    Added the connection to a real fhir store instead

--- orchestrator/careplancontributor/taskengine/validation.go
+++ orchestrator/careplancontributor/taskengine/validation.go
@@ -25,88 +25,81 @@ type FHIRValidatorAPIQuestionnaireResponseValidator struct {
 	URL    string
 }
 
-type CliContext struct {
-	SV     string   `json:"sv"`
-	IG     []string `json:"ig"`
-	Locale string   `json:"locale"`
-}
-
-type FileToValidate struct {
-	FileName    string `json:"fileName"`
-	FileContent string `json:"fileContent"`
-	FileType    string `json:"fileType"`
-}
-
-type ValidateQuestionnaireRequest struct {
-	CliContext      CliContext       `json:"cliContext"`
-	FilesToValidate []FileToValidate `json:"filesToValidate"`
-}
-
 func (f FHIRValidatorAPIQuestionnaireResponseValidator) Validate(ctx context.Context, questionnaire *fhir.Questionnaire, questionnaireResponse *fhir.QuestionnaireResponse) (*fhir.OperationOutcome, error) {
 	questionnaireJson, err := json.Marshal(questionnaire)
 	questionnaireResponseJson, err := json.Marshal(questionnaireResponse)
 
-	requestBody := ValidateQuestionnaireRequest{
-		CliContext: CliContext{
-			SV:     "4.0.1",
-			IG:     []string{"hl7.fhir.us.core#4.0.0"},
-			Locale: "en",
-		},
-		FilesToValidate: []FileToValidate{
-			{
-				FileName:    "questionnaire.json",
-				FileContent: string(questionnaireJson),
-				FileType:    "json",
-			},
-			{
-				FileName:    "questionnaireResponse.json",
-				FileContent: string(questionnaireResponseJson),
-				FileType:    "json",
-			},
-		},
-	}
+	_, err = f.postQuestionnaireResource(ctx, err, questionnaireJson, "/Questionnaire")
 
-	data, err := json.Marshal(requestBody)
+	bodyBytes, err := f.postQuestionnaireResource(ctx, err, questionnaireResponseJson, "/QuestionnaireResponse")
 	if err != nil {
-		return nil, fmt.Errorf("failed to marshal request body: %w", err)
+		return nil, fmt.Errorf("failed to post QuestionnaireResponse: %w", err)
+	}
+	var createdQuestionnaireResponse fhir.QuestionnaireResponse
+	if err = json.Unmarshal(bodyBytes, &createdQuestionnaireResponse); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal QuestionnaireResponse: %w", err)
 	}
 
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, f.URL, bytes.NewReader(data))
+	path := "QuestionnaireResponse/" + *createdQuestionnaireResponse.Id + "/$validate?questionnaire=required&display-issues-are-warnings=false"
+
+	validationRequest, err := http.NewRequestWithContext(ctx, http.MethodGet, f.URL+path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create validation request: %w", err)
+	}
+	validationRequest.Header.Add("Content-Type", "application/fhir+json")
+
+	response, err := f.client.Do(validationRequest)
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %w", err)
+		return nil, fmt.Errorf("failed to send validation request: %w", err)
 	}
 
-	request.Header.Add("Content-Type", "application/json")
-
-	response, err := f.client.Do(request)
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %w", err)
+		return nil, fmt.Errorf("failed to read error response body: %w", err)
 	}
-
-	bodyBytes, err := io.ReadAll(response.Body)
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		if err != nil {
-			return nil, fmt.Errorf("failed to read error response body: %w", err)
-		}
-		return nil, fmt.Errorf("validation failed with status code %d: %s", response.StatusCode, string(bodyBytes))
-	}
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %w", err)
+		return nil, fmt.Errorf("failed to post with status code %d: %s", response.StatusCode, string(bodyBytes))
 	}
 
 	var bundle fhir.OperationOutcome
-	if err = json.Unmarshal(bodyBytes, &bundle); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal validation result: %w", err)
+	if err = json.Unmarshal(body, &bundle); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal OperationOutcome: %w", err)
 	}
+
 	for _, issue := range bundle.Issue {
 		if issue.Severity == fhir.IssueSeverityFatal || issue.Severity == fhir.IssueSeverityError {
 			return &bundle, errors.New("validation failed, see OperationOutcome for details")
 		}
 	}
 	return &bundle, nil
+}
 
+func (f FHIRValidatorAPIQuestionnaireResponseValidator) postQuestionnaireResource(ctx context.Context, err error, questionnaireJson []byte, path string) ([]byte, error) {
+	url := f.URL + path
+	post, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(questionnaireJson))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+	post.Header.Add("Content-Type", "application/fhir+json")
+
+	response, err := f.client.Do(post)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+	bodyBytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read error response body: %w", err)
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusCreated {
+		return nil, fmt.Errorf("failed to post with status code %d: %s", response.StatusCode, string(bodyBytes))
+	}
+	return bodyBytes, nil
 }
--- orchestrator/careplancontributor/taskengine/validation_test.go
+++ orchestrator/careplancontributor/taskengine/validation_test.go
@@ -6,16 +6,14 @@ import (
 	"github.com/SanteonNL/orca/orchestrator/careplancontributor/taskengine/testdata"
 	"github.com/stretchr/testify/require"
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
-	"io"
 	"net/http"
-	"os"
 	"testing"
 )
 
 func TestFHIRValidatorAPIQuestionnaireResponseValidator_Validate(t *testing.T) {
 	ctx := context.Background()
 
-	validator := FHIRValidatorAPIQuestionnaireResponseValidator{URL: "http://localhost:3500/validate", client: &http.Client{}}
+	validator := FHIRValidatorAPIQuestionnaireResponseValidator{URL: "http://localhost:9090/fhir/", client: &http.Client{}}
 	t.Run("ok", func(t *testing.T) {
 		questionnaireData, err := testdata.FS.ReadFile("questionnaire-heartfailure-enrollment.json")
 		require.NoError(t, err)
@@ -52,30 +50,4 @@ func TestFHIRValidatorAPIQuestionnaireResponseValidator_Validate(t *testing.T) {
 		oocJSON, _ := json.Marshal(ooc)
 		require.Contains(t, string(oocJSON), "Item has answer, even though it is not enabled (item id = '170292e5-3163-43b4-88af-affb3e4c27ab')")
 	})
-}
-
-func downloadFHIRValidatorCLI() error {
-	validatorJar := "bin/fhir_validator_cli-6.5.7.jar"
-	// If not exists, download it from https://github.com/hapifhir/org.hl7.fhir.core/releases/download/6.5.7/validator_cli.jar
-	if _, err := os.Stat(validatorJar); os.IsNotExist(err) {
-		// download it
-		if err := os.MkdirAll("bin", 0755); err != nil {
-			return err
-		}
-		println("Downloading FHIR Validator CLI...")
-		httpResponse, err := http.Get("https://github.com/hapifhir/org.hl7.fhir.core/releases/download/6.5.7/validator_cli.jar")
-		if err != nil {
-			return err
-		}
-		defer httpResponse.Body.Close()
-		file, err := os.Create(validatorJar)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		if _, err := io.Copy(file, httpResponse.Body); err != nil {
-			return err
-		}
-	}
-	return nil
 }
