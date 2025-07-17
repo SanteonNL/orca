c7cf1b9c34c57ad81b32dd251e23077350cb798e
Jethro Sloan <jdksloan@gmail.com>
2025-07-17 09:48:47 +0200

    Added a nice check for the front end messages

--- frontend/app/enrollment/new/components/enroll-in-cps-button.tsx
+++ frontend/app/enrollment/new/components/enroll-in-cps-button.tsx
@@ -115,11 +115,15 @@ export default function EnrollInCpsButton({className}: Props) {
                 {validationErrors && (
                     <div className='text-red-500 mb-2'>
                         <h3 className='font-semibold'>Validation Errors:</h3>
-                            {validationErrors?.map((issue, index) => (
-                                <p key={index}>
-                                    {issue.diagnostics || "Unknown error"}
-                                </p>
-                            ))}
+                        {validationErrors.every(m => !m.diagnostics) ? ('An unknown error occurred') :
+                            (
+                                validationErrors?.filter(issue => issue.diagnostics).map((issue, index) => (
+                                    <p key={index}>
+                                        {issue.diagnostics || "Unknown error"}
+                                    </p>
+                                ))
+                            )
+                        }
                     </div>
                 )}
             </div>
