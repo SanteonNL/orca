cab61fa2bfb0973a779c48a73051f0edae69aeff
Rein Krul <info@reinkrul.nl>
2025-07-22 16:26:15 +0200

    DEV-716: Attempted fix of Epic SMART on FHIR launch

--- orchestrator/careplancontributor/applaunch/smartonfhir/service_test.go
+++ orchestrator/careplancontributor/applaunch/smartonfhir/service_test.go
@@ -179,7 +179,7 @@ func TestService(t *testing.T) {
 			println(string(responseData))
 			require.Equal(t, http.StatusOK, httpResponse.StatusCode, "Expected status code 200 OK for app launch with launch parameter")
 			// Assert captured authorization request parameters
-			require.Equal(t, []string{"openid fhirUser user/Patient.r user/Practitioner.r launch"}, capturedScope)
+			require.Equal(t, []string{"openid fhirUser launch"}, capturedScope)
 			require.Equal(t, []string{issuerURL}, capturedAudience)
 			require.Equal(t, []string{clientID}, capturedClientID)
 			require.Equal(t, []string{"test-launch"}, capturedLaunchParam)
