c276c7878293632238d28a96d88cf0b6c7cdf45e
Rein Krul <info@reinkrul.nl>
2025-09-10 18:00:06 +0200

    Fixed tests

--- frontend/app/utils/mapping.ts
+++ frontend/app/utils/mapping.ts
@@ -85,5 +85,7 @@ export function codingLabel(coding: coding): string | undefined {
 }
 
 export function titleCase(str: string): string {
+    if (str.length === 0) return str;
+    if (str.length === 1) return str.toUpperCase();
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
