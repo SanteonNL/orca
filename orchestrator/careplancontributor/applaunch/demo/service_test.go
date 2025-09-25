5d8b670df778577bcbda2c5765eadfb76ebf2f60
Rein Krul <info@reinkrul.nl>
2025-09-25 10:22:43 +0200

    DEV-889: Prevent path traversal of FHIR requests

--- orchestrator/careplancontributor/applaunch/demo/service_test.go
+++ orchestrator/careplancontributor/applaunch/demo/service_test.go
@@ -1,6 +1,12 @@
 package demo
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
 	fhirclient "github.com/SanteonNL/go-fhir-client"
 	"github.com/SanteonNL/orca/orchestrator/careplancontributor/applaunch/session"
 	"github.com/SanteonNL/orca/orchestrator/cmd/profile"
@@ -8,11 +14,6 @@ import (
 	"github.com/SanteonNL/orca/orchestrator/lib/auth"
 	"github.com/SanteonNL/orca/orchestrator/lib/coolfhir"
 	"github.com/SanteonNL/orca/orchestrator/lib/must"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"testing"
-	"time"
 
 	"github.com/SanteonNL/orca/orchestrator/globals"
 	"github.com/SanteonNL/orca/orchestrator/lib/test"
@@ -81,6 +82,24 @@ func TestService_handle(t *testing.T) {
 		require.Equal(t, "ServiceRequest/b", sessionData.GetByType("ServiceRequest").Path)
 		require.Equal(t, "Practitioner/c", sessionData.GetByType("Practitioner").Path)
 		require.Equal(t, "unit-test-system|10", *sessionData.TaskIdentifier)
+	})
+	t.Run("path traversal", func(t *testing.T) {
+		sessionManager := user.NewSessionManager[session.Data](time.Minute)
+		service := Service{
+			sessionManager: sessionManager, orcaPublicURL: must.ParseURL("/"),
+			frontendLandingUrl: must.ParseURL("/cpc/"),
+			ehrFHIRClientFactory: func(_ *url.URL, _ *http.Client) fhirclient.Client {
+				return ehrFHIRClient
+			},
+			profile: profile.TestProfile{Principal: auth.TestPrincipal1},
+			tenants: tenantCfg,
+		}
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest("GET", "/demo-app-launch?patient=../Patient/a&serviceRequest=../ServiceRequest/b&practitioner=../Practitioner/c&tenant="+tenant.ID+"&taskIdentifier=unit-test-system|10", nil)
+
+		service.handle(response, request)
+
+		require.Equal(t, http.StatusBadRequest, response.Code)
 	})
 	t.Run("subpath base URL", func(t *testing.T) {
 		sessionManager := user.NewSessionManager[session.Data](time.Minute)
