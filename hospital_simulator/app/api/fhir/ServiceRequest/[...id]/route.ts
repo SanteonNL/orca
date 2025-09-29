44693be5a92572a8038de63e310a54e486c7e26d
PatAkil <pakil@xebia.com>
2025-09-29 13:56:06 +0200

    DEV-988: fixes build

--- hospital_simulator/app/api/fhir/ServiceRequest/[...id]/route.ts
+++ hospital_simulator/app/api/fhir/ServiceRequest/[...id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-type Params = Promise<{ id: string }>
+type Params = Promise<{ id: string[] }>
 
 export async function PATCH(req: NextRequest, { params }: { params: Params }) {
     try {
@@ -11,7 +11,8 @@ export async function PATCH(req: NextRequest, { params }: { params: Params }) {
         }
 
         const patchData = await req.json(); // Extract the patch data from the request body
-        const { id } = await params; // Extract the ServiceRequest ID from the URL
+        const { id: idArray } = await params; // Extract the ServiceRequest ID array from the URL
+        const id = idArray[0]; // Get the first element since we expect a single ID
 
         const patchResponse = await fetch(`${fhirBaseUrl}/ServiceRequest/${id}`, {
             method: 'PATCH',
