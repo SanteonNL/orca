a374dffa54708689c3effa6f088eb58355967c64
J Teeuwissen <j.teeuwissen@zorgbijjou.nl>
2026-03-10 13:22:13 +0100

    revert practitioner id error (#972)

--- orchestrator/careplancontributor/oidc/op/service.go
+++ orchestrator/careplancontributor/oidc/op/service.go
@@ -121,10 +121,9 @@ func userFromSession(sessionData *session.Data) (*UserDetails, error) {
 		userDetails.Name = coolfhir.FormatHumanName(name)
 		break
 	}
-	if practitioner.Id == nil {
-		return nil, errors.New("no id found in practitioner resource")
+	if practitioner.Id != nil {
+		userDetails.ID = *practitioner.Id
 	}
-	userDetails.ID = *practitioner.Id
 	for _, identifier := range practitioner.Identifier {
 		userDetails.ID = *identifier.Value
 	}
