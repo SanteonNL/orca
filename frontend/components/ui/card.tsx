48f808c45ac224736d8741a17e189574bb80fd5b
J Teeuwissen <j.teeuwissen@zorgbijjou.nl>
2026-04-20 15:21:32 +0200

    Removed dead code to improve coverage (#978)

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -76,4 +76,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader,    CardContent }
